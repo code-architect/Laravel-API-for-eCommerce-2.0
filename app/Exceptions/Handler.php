32a961cf9bc2621f9b03f22adeb2c957363ef36c
code-architect <codearchitectdeveloper@gmail.com>
2018-07-11 03:59:58 +0530

    Handling wrong method requests in routes or MethodNotAllowedHttpException exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,6 +9,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
@@ -70,12 +71,15 @@ class Handler extends ExceptionHandler
             return $this->errorResponse($exception->getMessage(), 403);
         }
 
+        // Content not found exception handled
         if ($exception instanceof NotFoundHttpException) {
             return $this->errorResponse('This specified url cannot be found.', 404);
         }
 
-
-
+        // Handling unauthorized users exception
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('This specified method for the request is invalid.', 405);
+        }
 
 
         return parent::render($request, $exception);
