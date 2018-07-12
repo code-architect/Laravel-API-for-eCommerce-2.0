978d8000f02b2ff66261f575e574c64395c236b1
code-architect <codearchitectdeveloper@gmail.com>
2018-07-12 22:55:40 +0530

    Implimenting Implicit model binding with routes and methods, in here Just the User and Buyer

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -2,11 +2,19 @@
 
 namespace App\Models;
 
+use App\Scopes\BuyerScope;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Transaction;
 
 class Buyer extends User
 {
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new BuyerScope);
+    }
 
     // -------------------- Relations -----------------------------------------------//
     public function transactions()
