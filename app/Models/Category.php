1d89a9718c1a1503880433b4fdd40373b04d3e8b
code-architect <codearchitectdeveloper@gmail.com>
2018-07-13 02:36:27 +0530

    Implimenting Soft delete for models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Product;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
     protected $fillable = ['name', 'description'];
 
 
