5fe17b8aedc9f6215dd461f0c39c01ab1e444ebb
code-architect <codearchitectdeveloper@gmail.com>
2018-07-29 23:03:29 +0530

    created SellerBuyer controller, Displaying all the buyers (unique) of a single seller

---
+++ app/Http/Controllers/Seller/SellerBuyerController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Models\Seller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class SellerBuyerController extends ApiController
+{
+    /**
+     * Displaying all the buyers (unique) of a single seller.
+     *
+     * @param $seller $seller
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Seller $seller)
+    {
+        $categories = $seller->products()
+                            ->whereHas('transactions')
+                            ->with('transactions.buyer')
+                            ->get()
+                            ->pluck('transactions')
+                            ->collapse()
+                            ->pluck('buyer')
+                            ->unique('id')
+                            ->values();
+
+        return $this->showAll($categories);
+    }
+
+
+}
